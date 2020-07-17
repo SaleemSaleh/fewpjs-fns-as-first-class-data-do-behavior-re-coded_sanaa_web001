98e4bb1e9dd54e74c13da0fdfdb7dd0ac771d0da
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-17 15:06:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function greet(timeString){
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(String) {
-document.getElementById("greeting").textContent = "TEST" ;
+document.getElementById("greeting").innerText = ` ${String}` ;
 }
