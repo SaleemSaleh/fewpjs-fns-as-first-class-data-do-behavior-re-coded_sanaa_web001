ec141ff0aaca1d9d0c79c7b2cbdefc703814fba5
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-17 14:42:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,28 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  let htime = timeString.split(':').
+  let itime = parseInt(htime);
+  return itime ;
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(time) {
+
+  let greatingMass = document.getElementById('greeting');
+  if (time < 12) {
+    greatingMass.innerHTML = "Good Morning"
+
+  }else if (time > 12 && time < 17) {
+    greatingMass.innerHTML = "Good Afternoon";
+  }
+  else {
+    greatingMass.innerHTML = "Good Evening";
+  }
+
+}
